78892f5cbb0f671873181ce523d3e0f4ae6e4c22
SzipeRy <thannyapuesh@gmail.com>
2025-09-07 15:56:05 +0700

    07-09-2025

--- d66_q1a_booking/d66_q1a_booking.cpp
+++ d66_q1a_booking/d66_q1a_booking.cpp
@@ -7,7 +7,7 @@ int main(void)
     std::ios_base::sync_with_stdio(false); std::cin.tie(0);
 
     int n, m;
-    std::map<std::string, std::vector<std::string>> flightSeat;
+    std::map<std::string, std::vector<std::pair<std::string, bool>>> flightSeat;
     
 
     std::cin >> n >> m;
@@ -16,7 +16,7 @@ int main(void)
     {
         std::string flight, seat;
         std::cin >> flight >> seat;
-        flightSeat[flight].push_back(seat);
+        flightSeat[flight].push_back({seat, true});
     }
 
     for (auto x : flightSeat)
